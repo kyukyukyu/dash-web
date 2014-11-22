aa02921b51b20eb4ad26cb354242243bca6b8e8c
Sanggyu Nam <pokeplus@gmail.com>
2014-11-22 22:44:11 +0900

    Add ClassSearchCtrl controller
    * This controller depends on Classes service.
    * This controller manages search options and keyword, and sends requests
      to backend with the options via Classes service to search the classes.
    * This controller retrieves class search result and groups it by
      subject.
    * Fixture file for classes was modified.

---
+++ app/scripts/controllers/class_search.js
@@ -0,0 +1,95 @@
+'use strict';
+
+/**
+ * Parse the keyword into query object.
+ * @param {String} keyword Keyword to parse.
+ */
+var parseKeyword = function (keyword) {
+  if (typeof keyword !== 'string') {
+    throw new TypeError('keyword should be string');
+  }
+
+  var queryObj = {};
+  var arr = keyword.split(/\b(name|instructor|subject):/);
+
+  for (var i = 0, str; i < arr.length; ++i) {
+    var key, value;
+
+    str = arr[i];
+
+    if (/^(name|instructor|subject)$/.test(str)) {
+      // get the value for specified property
+      value = arr[++i];
+      switch (str) {
+        case 'name':
+              key = 'name';
+              break;
+        case 'instructor':
+              key = 'instructor';
+              break;
+        case 'subject':
+              key = 'subject';
+      }
+    } else {
+      key = 'name';
+      value = str;
+    }
+
+    value = value.trim();
+    if (!value) {
+      continue;
+    }
+    queryObj[key] = value.trim();
+  }
+
+  return queryObj;
+};
+
+/**
+ * @ngdoc function
+ * @name dashApp.controller:ClassSearchCtrl
+ * @description
+ * # ClassSearchCtrl
+ * Controller of the dashApp
+ */
+angular.module('dashApp')
+  .controller('ClassSearchCtrl', function ($scope, Classes) {
+    $scope.type = 'all';
+    $scope.keyword = '';
+    $scope.searchResult = null;
+
+    $scope.searchClasses = function () {
+      /* jshint camelcase: false */
+      var queryObj = {};
+
+      angular.extend(queryObj, parseKeyword($scope.keyword));
+
+      queryObj.type = $scope.type;
+      if ($scope.type === 'general') {
+        queryObj.area_id = $scope.area_id;
+      } else if ($scope.type === 'major') {
+        queryObj.grade = $scope.grade;
+        queryObj.department_id = $scope.department_id;
+      }
+
+      return Classes.getList(queryObj).then(function (list) {
+        var classGroups = {},
+            searchResult = [];
+
+        var classGroup;
+        angular.forEach(list, function (c) {
+          if (!(c.subject_id in classGroups)) {
+            classGroups[c.subject_id] = {
+              subject: angular.copy(c.subject),
+              classes: []
+            };
+            searchResult.push(classGroups[c.subject_id]);
+          }
+          classGroup = classGroups[c.subject_id];
+          classGroup.classes.push(c);
+        });
+
+        $scope.searchResult = searchResult;
+      });
+    };
+  });
---
+++ test/spec/controllers/class_search.js
@@ -0,0 +1,187 @@
+/* global getJSONFixture, URI */
+'use strict';
+
+describe('Controller: ClassSearchCtrl', function () {
+  jasmine.getJSONFixtures().fixturesPath = 'base/test/mock';
+
+  var fxClasses = getJSONFixture('classes.json');
+
+  // load the controller's module
+  beforeEach(module('dashApp'));
+
+  var $httpBackend, $timeout, ClassSearchCtrl, scope;
+  var backendUrlRegex = /^\/api\/classes(\?(.*))?$/;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function (_$httpBackend_, _$timeout_, $controller, $rootScope) {
+    /* jshint unused: false */
+
+    $httpBackend = _$httpBackend_;
+    $httpBackend.when('GET', backendUrlRegex).respond(fxClasses);
+    $timeout = _$timeout_;
+    scope = $rootScope.$new();
+    ClassSearchCtrl = $controller('ClassSearchCtrl', {
+      $scope: scope
+    });
+  }));
+
+  afterEach(function () {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  describe('should search classes', function () {
+    /* jshint camelcase: false */
+
+    // Set request expectations
+    var expectedQueryObj;
+    beforeEach(function () {
+      var self = this;
+
+      expectedQueryObj = {};
+      $httpBackend.expect('GET', backendUrlRegex)
+        .respond(function () {
+          var uri = new URI(arguments[1]);
+          self.queryObj = uri.query(true);
+
+          return [200, fxClasses, {}, 'OK'];
+        });
+    });
+
+    /**
+     * createSpecGroup returns a function that receives a group of test specs
+     * as argument, copies it as a subgroup with given information.
+     * @param {String} description The description for new subgroup of test
+     *                 specs.
+     * @param {String} property The property which should be written in search
+     *                 box.
+     * @param {String} queryObjKey The key for object that represents the
+     *                 expected query.
+     * @param {String|Number} value The value for option.
+     * @returns {Function(specs)} A function that can be run in the middle of
+     *                            test specs. This accepts a group of test
+     *                            specs as argument, and creates a new
+     *                            subgroup of test specs with beforeEach()
+     *                            block. In this beforeEach() block,
+     *                            scope.keyword and expectedQueryObj is
+     *                            modified.
+     */
+    var createSpecGroup = function (description, property, queryObjKey, value) {
+      return function (specs) {
+        describe(description, function () {
+          beforeEach(function () {
+            scope.keyword = (scope.keyword || '') + ' ' + property + ':' + value;
+            expectedQueryObj[queryObjKey] = value;
+          });
+
+          specs.apply(this);
+        });
+
+        specs.apply(this);
+      };
+    };
+
+    var commonSpecs = function () {
+      it('of every type', function () {
+        expectedQueryObj.type = 'all';
+      });
+
+      it('of general education', function () {
+        scope.type = 'general';
+        expectedQueryObj.type = 'general';
+      });
+
+      it('of general education by its area', function () {
+        scope.type = 'general';
+        scope.area_id = 2;
+        expectedQueryObj.type = 'general';
+        expectedQueryObj.area_id = '2';
+      });
+
+      it('of major education', function () {
+        scope.type = 'major';
+        expectedQueryObj.type = 'major';
+      });
+
+      it('of major education by its target grade', function () {
+        scope.type = 'major';
+        scope.grade = 3;
+        expectedQueryObj.type = 'major';
+        expectedQueryObj.grade = '3';
+      });
+
+      it('of major education by its department', function () {
+        scope.type = 'major';
+        scope.department_id = 1;
+        expectedQueryObj.type = 'major';
+        expectedQueryObj.department_id = '1';
+      });
+
+      it('of major education by its target grade and department', function () {
+        scope.type = 'major';
+        scope.grade = 3;
+        scope.department_id = 1;
+        expectedQueryObj.type = 'major';
+        expectedQueryObj.grade = '3';
+        expectedQueryObj.department_id = '1';
+      });
+    };
+
+    var specsWithName = createSpecGroup('by its name', 'name', 'name', 'understanding');
+    var specsWithInstructor = createSpecGroup('by its instructor', 'instructor', 'instructor', 'Yun, sunhee');
+    var specsWithCode = createSpecGroup('by its subject code', 'subject', 'subject', 'GEN4091');
+
+    specsWithName(function () {
+      specsWithInstructor(function () {
+        specsWithCode(commonSpecs);
+      });
+    });
+
+    afterEach(function () {
+      scope.searchClasses();
+      $httpBackend.flush();
+      expect(this.queryObj).toEqual(expectedQueryObj);
+    });
+  });
+
+  it('should group the search result by its subject', function () {
+    /* jshint camelcase: false */
+    scope.searchClasses();
+
+    $httpBackend.flush();
+
+    var searchResult = angular.copy(scope.searchResult);
+    expect(searchResult.length).toBe(4);
+
+    var expectedResult = [
+      {subjectId: 2, classIds: [3]},
+      {subjectId: 4, classIds: [5]},
+      {subjectId: 3, classIds: [4]},
+      {subjectId: 1, classIds: [1, 2]}
+    ];
+
+    searchResult.sort(function (a, b) {
+      return a.subject.name.localeCompare(b.subject.name);
+    });
+
+    angular.forEach(searchResult, function (classGroup, i) {
+      var expectedClassGroup = expectedResult[i];
+      expect(classGroup.subject.id).toBe(expectedClassGroup.subjectId);
+      expect(classGroup.classes.length).toBe(expectedClassGroup.classIds.length);
+
+      var classes = angular.copy(classGroup.classes);
+      classes.sort(function (a, b) {
+        if (a.id > b.id) {
+          return 1;
+        }
+        if (a.id < b.id) {
+          return -1;
+        }
+        return 0;
+      });
+      angular.forEach(classes, function (c, j) {
+        expect(c.id).toBe(expectedClassGroup.classIds[j]);
+      });
+    });
+  });
+});
