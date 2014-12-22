a05d4a20d9beb1accd454adbffdf601190a54fda
Sanggyu Nam <pokeplus@gmail.com>
2014-12-22 13:11:54 +0900

    Conform to REST API result format
    * For result of collection API, properties named `num_results`,
      `page`, and `num_pages` were added.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,5 +33,22 @@ angular
         controller: 'CampusListCtrl'
       });
 
-    RestangularProvider.setBaseUrl('/api');
+    RestangularProvider
+      .setBaseUrl('/api')
+      .addResponseInterceptor(function (data, operation) {
+        var extractedData;
+        if (operation === 'getList') {
+          /* jshint sub: true */
+          extractedData = data.objects;
+          extractedData.numResults = data['num_results'];
+          extractedData.page = data.page;
+          extractedData.numPages = data['num_pages'];
+        } else {
+          extractedData = data;
+        }
+        return extractedData;
+      });
+  })
+  .config(function (uiSelectConfig) {
+    uiSelectConfig.theme = 'bootstrap';
   });
