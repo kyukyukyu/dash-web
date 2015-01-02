92f131d3797f8df0409e8bc2c17bb5d8da216559
Sanggyu Nam <pokeplus@gmail.com>
2015-01-02 21:44:22 +0900

    Enable `Campuses` to hold selected campus for app
    * Selected campus for app means the campus entity used to search courses
      and create timetables, and it is used globally in the app.

--- app/scripts/services/campuses.js
+++ app/scripts/services/campuses.js
@@ -8,6 +8,28 @@
  * Factory in the dashApp.
  */
 angular.module('dashApp')
-  .factory('Campuses', function (Restangular) {
-    return Restangular.service('campuses');
+  .factory('Campuses', function ($q, Restangular) {
+    var service = Restangular.service('campuses');
+    var selectedCampus = null;
+    service.setSelectedCampus = function (campusOrId) {
+      var promise;
+
+      if (angular.isNumber(campusOrId)) {
+        // function called with ID
+        promise = service.one(campusOrId).get();
+      } else {
+        // function called with object
+        promise = $q.when(campusOrId);
+      }
+
+      return promise.then(function (campus) {
+        selectedCampus = campus;
+        return selectedCampus;
+      });
+    };
+    service.getSelectedCampus = function () {
+      return selectedCampus;
+    };
+
+    return service;
   });
--- test/spec/services/campuses.js
+++ test/spec/services/campuses.js
@@ -12,10 +12,67 @@ describe('Service: Campuses', function () {
   beforeEach(module('dashApp'));
 
   // instantiate service
-  var $httpBackend, Campuses;
-  beforeEach(inject(function (_$httpBackend_, _Campuses_) {
+  var $httpBackend, $timeout, Campuses;
+  beforeEach(inject(function (_$httpBackend_, _$timeout_, _Campuses_) {
     $httpBackend = _$httpBackend_;
+    $timeout = _$timeout_;
+    $httpBackend.when('GET', '/api/campuses').respond(mockCampuses);
+    $httpBackend.when('GET', '/api/campuses/1').respond(mockCampuses.objects[0]);
     Campuses = _Campuses_;
   }));
+
+  afterEach(function () {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('should get the list of campuses', function () {
+    $httpBackend.expect('GET', '/api/campuses');
+    var campusList = Campuses.getList().$object;
+    $httpBackend.flush();
+
+    expect(campusList.length).toBe(2);
+
+    var campus;
+    campus = campusList[0];
+    expect(campus.id).toBe(1);
+    expect(campus.name).toBe('HYU Seoul');
+    campus = campusList[1];
+    expect(campus.id).toBe(2);
+    expect(campus.name).toBe('HYU ERICA');
+  });
+
+  it('should get single campus', function () {
+    $httpBackend.expect('GET', '/api/campuses/1');
+    var campus = Campuses.one(1).get().$object;
+    $httpBackend.flush();
+
+    expect(campus.id).toBe(1);
+    expect(campus.name).toBe('HYU Seoul');
+  });
+
+  it('should set an campus object as selected campus for the app', function () {
+    $httpBackend.expect('GET', '/api/campuses/1');
+    var campus = Campuses.one(1).get().$object;
+    $httpBackend.flush();
+
+    Campuses.setSelectedCampus(campus);
+    $timeout.flush();
+
+    var selectedCampus = Campuses.getSelectedCampus();
+    expect(selectedCampus.id).toBe(1);
+    expect(selectedCampus.name).toBe('HYU Seoul');
+  });
+
+  it('should set selected campus for the app with its id', function () {
+    $httpBackend.expect('GET', '/api/campuses/1');
+    Campuses.setSelectedCampus(1);
+    $httpBackend.flush();
+    $timeout.flush();
+
+    var selectedCampus = Campuses.getSelectedCampus();
+    expect(selectedCampus.id).toBe(1);
+    expect(selectedCampus.name).toBe('HYU Seoul');
+  });
 
 });
