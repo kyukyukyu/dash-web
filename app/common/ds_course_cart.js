caa9e5905b57e9a123c3da12e8c0ac17caec1698
Sanggyu Nam <pokeplus@gmail.com>
2015-02-16 17:40:40 +0900

    Create directive named dsCourseCart
    * This directive element shows what's in the course cart.

---
+++ app/common/ds_course_cart.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name dashApp.common:dsCourseCart
+ * @description
+ * # dsCourseCart
+ *
+ * dsCourseCart shows what's in the course cart.
+ */
+angular.module('dashApp.common')
+  .directive('dsCourseCart', function (CourseCart) {
+    /* jshint unused: false */
+
+    function postLink(scope, element, attrs) {
+      // This variable will be synced with courseGroups of CourseCart.
+      scope.courseGroups = CourseCart.getCourseGroups();
+    }
+
+    return {
+      templateUrl: 'common/ds_course_cart.tpl.html',
+      restrict: 'E',
+      scope: {},
+      link: postLink
+    };
+  });
---
+++ test/spec/common/ds_course_cart.js
@@ -0,0 +1,144 @@
+'use strict';
+
+describe('Directive: dsCourseCart', function () {
+
+  // load the directive's module
+  beforeEach(module('dashApp.common'));
+  beforeEach(module('common/ds_subject.tpl.html'));
+  beforeEach(module('common/ds_course_cart.tpl.html'));
+
+  // load mock modules
+  beforeEach(module('dashApp.mock.campuses'));
+  beforeEach(module('dashApp.mock.courses'));
+
+  // load mock data
+  var mockMajorSubject, mockMajorCourses, mockGeneralSubject, mockGeneralCourses;
+  beforeEach(function () {
+    var mockDataSource = getJSONFixture('courses.json');
+    mockMajorSubject = mockDataSource.objects[0].subject;
+    mockGeneralSubject = mockDataSource.objects[4].subject;
+    mockMajorCourses = mockDataSource.objects.slice(0, 2);
+    mockGeneralCourses = mockDataSource.objects.slice(4, 5);
+  });
+
+  // load dependency
+  var $rootScope,
+    $httpBackend,
+    Courses,
+    CourseCart;
+  beforeEach(inject(function (_$rootScope_, _$httpBackend_, _Courses_, _CourseCart_) {
+    $rootScope = _$rootScope_;
+    $httpBackend = _$httpBackend_;
+    Courses = _Courses_;
+    CourseCart = _CourseCart_;
+  }));
+
+  // flush HTTP request made by Campuses service
+  beforeEach(function () { $httpBackend.flush(1); });
+
+  // make sure there is no outstanding http expectation/request
+  afterEach(function () {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  function addToCart(courseId) {
+    var course = Courses.get(courseId).$object;
+    $httpBackend.flush();
+    $timeout.flush();
+    CourseCart.add(course);
+    return course;
+  }
+
+  function removeFromCart(courseId) {
+    var course = Courses.get(courseId).$object;
+    $httpBackend.flush();
+    $timeout.flush();
+    CourseCart.remove(course);
+    return course;
+  }
+
+  var $compile, $timeout;
+  var element,
+    scope;
+
+  function compileElement(scope) {
+    var element = angular.element(
+      '<ds-course-cart></ds-course-cart>'
+    );
+    element = $compile(element)(scope);
+    $timeout.flush();
+
+    return element;
+  }
+
+  function assertDirective(element, courseGroups) {
+    var subjectElems = element.find('ds-subject');
+    expect(subjectElems.length).toBe(courseGroups.length);
+
+    angular.forEach(courseGroups, function (courseGroup, i) {
+      var subjectElem = angular.element(subjectElems[i]);
+      var isolateScope = subjectElem.isolateScope();
+
+      expect(isolateScope.subject).toEqual(courseGroup.subject);
+      expect(isolateScope.courses).toEqual(courseGroup.courses);
+    });
+  }
+
+  beforeEach(inject(function (_$compile_, _$timeout_, $rootScope) {
+    $compile = _$compile_;
+    $timeout = _$timeout_;
+    scope = $rootScope.$new();
+  }));
+
+  it('should show course groups in cart', function () {
+    var courseGroups;
+
+    addToCart(1);
+    addToCart(2);
+    addToCart(3);
+    courseGroups = CourseCart.getCourseGroups();
+
+    element = compileElement(scope);
+    assertDirective(element, courseGroups);
+  });
+
+  it('should react to changes in cart', function () {
+    var courseGroups;
+
+    addToCart(1);
+    courseGroups = CourseCart.getCourseGroups();
+
+    element = compileElement(scope);
+    assertDirective(element, courseGroups);
+
+    // this adds a course object in existing course group in the cart
+    addToCart(2);
+    $rootScope.$digest();
+
+    courseGroups = CourseCart.getCourseGroups();
+    assertDirective(element, courseGroups);
+
+    // this adds a course object in new course group in the cart
+    addToCart(3);
+    $rootScope.$digest();
+
+    courseGroups = CourseCart.getCourseGroups();
+    assertDirective(element, courseGroups);
+
+    // this removes a course group in the cart
+    removeFromCart(3);
+    $rootScope.$digest();
+
+    courseGroups = CourseCart.getCourseGroups();
+    assertDirective(element, courseGroups);
+
+    // this removes a course object in course group in the cart
+    removeFromCart(2);
+    $rootScope.$digest();
+
+    courseGroups = CourseCart.getCourseGroups();
+    assertDirective(element, courseGroups);
+  });
+
+});
