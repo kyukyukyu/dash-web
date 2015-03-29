fb696aafc12b49a52a9db94818cf90fbfcd6f599
Sanggyu Nam <pokeplus@gmail.com>
2015-03-29 17:31:40 +0900

    modal-genopt: Create controller and add validation
    * See issue #9.

---
+++ app/create/generator_options.js
@@ -0,0 +1,124 @@
+(function () {
+  /* jshint latedef: nofunc */
+  'use strict';
+
+  /**
+   * @ngdoc object
+   * @name dashApp.create:GeneratorOptionsCtrl
+   * @requires dashApp.create:TimetableGenerator
+   * @description
+   * # GeneratorOptionsCtrl
+   * Controller for timetable generator options modal window.
+   */
+  angular.module('dashApp.create')
+    .controller('GeneratorOptionsCtrl', GeneratorOptionsCtrl);
+
+  var PROPERTY_NAMES = 'credits dailyClassCount weeklyClassCount'.split(' ');
+
+  /* @ngInject */
+  function GeneratorOptionsCtrl(TimetableGenerator) {
+    var vm = this;
+
+    vm.isValidRange = isValidRange;
+    vm.isValidBound = isValidBound;
+    vm.userInput = {};
+
+    var getParseFunc = _.memoize(_getParseFunc);
+
+    activate();
+
+    function activate() {
+      var options = TimetableGenerator.getOptions();
+      _.forEach(PROPERTY_NAMES, function (propertyName) {
+        var _propertyName = capitalize(propertyName);
+
+        _.forEach(['min', 'max'], function (boundPrefix) {
+          var boundPropName = boundPrefix + _propertyName;
+          vm.userInput[boundPropName] = options[boundPropName];
+        });
+      });
+    }
+
+    function capitalize(s) {
+      return s.charAt(0).toUpperCase() + s.slice(1);
+    }
+
+    /**
+     * @ngdoc method
+     * @name dashApp.create:GeneratorOptions#isValidRange
+     * @param {String} propertyName Name of property of which the range is to be checked.
+     * @returns {boolean} `true` if the range is valid, else `false`.
+     */
+    function isValidRange(propertyName) {
+      var _propertyName = capitalize(propertyName);
+      var parseFunc = getParseFunc(propertyName);
+
+      var boundary =
+          _(['min', 'max'])
+            .map(function (prefix) {
+              return vm.userInput[prefix + _propertyName];
+            })
+            .map(parseFunc)
+            .value();
+      var minBound = boundary[0];
+      var maxBound = boundary[1];
+      return (minBound === null || maxBound === null || minBound <= maxBound);
+    }
+
+    /**
+     * @ngdoc method
+     * @name dashApp.create:GeneratorOptions#isValidBound
+     * @param {String} propertyName Name of property which the bound is for.
+     * @param {String} bound Value to be checked if it is valid as bound for the property.
+     * @returns {boolean} `true` if the bound is valid, else `false`.
+     */
+    function isValidBound(propertyName, bound) {
+      var parseFunc = getParseFunc(propertyName);
+      var _bound = parseFunc(bound);
+
+      if (_.isNaN(_bound)) {
+        return false;
+      }
+      if (propertyName === 'weeklyClassCount') {
+        return (_bound === null || _bound > 0 && _bound <= 7);
+      } else {
+        return (_bound === null || _bound > 0);
+      }
+    }
+
+    function _getParseFunc(propertyName) {
+      var _parseFunc;
+
+      if (propertyName === 'credits') {
+        _parseFunc = _parseFloat;
+      } else {
+        _parseFunc = _parseInt;
+      }
+
+      function parseFunc(bound) {
+        if (bound === '' || bound === null) {
+          return null;
+        }
+        return _parseFunc(bound);
+      }
+
+      return parseFunc;
+    }
+
+    function _parseInt(s) {
+      if (/^[0-9]+$/.test(s)) {
+        return Number(s);
+      }
+
+      return NaN;
+    }
+
+    function _parseFloat(s) {
+      if (/^[0-9]*(\.([0-9]*)?)?$/.test(s)) {
+        return Number(s);
+      }
+
+      return NaN;
+    }
+  }
+})();
---
+++ test/spec/create/generator_options.js
@@ -0,0 +1,125 @@
+(function () {
+  /* jshint latedef: nofunc */
+  'use strict';
+
+  describe('Controller: GeneratorOptionsCtrl', function () {
+
+    var TimetableGenerator;
+    var GeneratorOptionsCtrl;
+
+    beforeEach(module('dashApp.create'));
+
+    // mock TimetableGenerator
+    beforeEach(function () {
+      TimetableGenerator = {};
+      TimetableGenerator.getOptions = getOptions;
+
+      var options = {
+        minCredits: null,
+        maxCredits: null,
+        minDailyClassCount: null,
+        maxDailyClassCount: null,
+        minWeeklyClassCount: null,
+        maxWeeklyClassCount: null
+      };
+
+      function getOptions() {
+        return options;
+      }
+    });
+
+    beforeEach(inject(function ($controller) {
+      GeneratorOptionsCtrl = $controller('GeneratorOptionsCtrl', {
+        'TimetableGenerator': TimetableGenerator
+      });
+    }));
+
+    it('should check if lower bound of credits <= upper bound of credits', function () {
+      GeneratorOptionsCtrl.userInput.minCredits = '';
+      GeneratorOptionsCtrl.userInput.maxCredits = '';
+      expect(GeneratorOptionsCtrl.isValidRange('credits')).toEqual(true);
+
+      GeneratorOptionsCtrl.userInput.minCredits = '';
+      GeneratorOptionsCtrl.userInput.maxCredits = '4';
+      expect(GeneratorOptionsCtrl.isValidRange('credits')).toEqual(true);
+
+      GeneratorOptionsCtrl.userInput.minCredits = '2';
+      GeneratorOptionsCtrl.userInput.maxCredits = '4';
+      expect(GeneratorOptionsCtrl.isValidRange('credits')).toEqual(true);
+
+      GeneratorOptionsCtrl.userInput.minCredits = '8';
+      GeneratorOptionsCtrl.userInput.maxCredits = '4';
+      expect(GeneratorOptionsCtrl.isValidRange('credits')).toEqual(false);
+
+      GeneratorOptionsCtrl.userInput.minCredits = '8';
+      GeneratorOptionsCtrl.userInput.maxCredits = '';
+      expect(GeneratorOptionsCtrl.isValidRange('credits')).toEqual(true);
+    });
+
+    it('should check if lower/upper bound of credits is valid', function () {
+      expect(GeneratorOptionsCtrl.isValidBound('credits', '')).toEqual(true);
+      expect(GeneratorOptionsCtrl.isValidBound('credits', '2.5')).toEqual(true);
+      expect(GeneratorOptionsCtrl.isValidBound('credits', '2')).toEqual(true);
+      expect(GeneratorOptionsCtrl.isValidBound('credits', '-1')).toEqual(false);
+    });
+
+    it('should check if lower bound of # of classes/day <= upper bound', function () {
+      GeneratorOptionsCtrl.userInput.minDailyClassCount = '';
+      GeneratorOptionsCtrl.userInput.maxDailyClassCount = '';
+      expect(GeneratorOptionsCtrl.isValidRange('dailyClassCount')).toEqual(true);
+
+      GeneratorOptionsCtrl.userInput.minDailyClassCount = '';
+      GeneratorOptionsCtrl.userInput.maxDailyClassCount = '4';
+      expect(GeneratorOptionsCtrl.isValidRange('dailyClassCount')).toEqual(true);
+
+      GeneratorOptionsCtrl.userInput.minDailyClassCount = '2';
+      GeneratorOptionsCtrl.userInput.maxDailyClassCount = '4';
+      expect(GeneratorOptionsCtrl.isValidRange('dailyClassCount')).toEqual(true);
+
+      GeneratorOptionsCtrl.userInput.minDailyClassCount = '8';
+      GeneratorOptionsCtrl.userInput.maxDailyClassCount = '4';
+      expect(GeneratorOptionsCtrl.isValidRange('dailyClassCount')).toEqual(false);
+
+      GeneratorOptionsCtrl.userInput.minDailyClassCount = '8';
+      GeneratorOptionsCtrl.userInput.maxDailyClassCount = '';
+      expect(GeneratorOptionsCtrl.isValidRange('dailyClassCount')).toEqual(true);
+    });
+
+    it('should check if lower/upper bound of # of classes/day is valid', function () {
+      expect(GeneratorOptionsCtrl.isValidBound('dailyClassCount', '')).toEqual(true);
+      expect(GeneratorOptionsCtrl.isValidBound('dailyClassCount', '2.5')).toEqual(false);
+      expect(GeneratorOptionsCtrl.isValidBound('dailyClassCount', '2')).toEqual(true);
+      expect(GeneratorOptionsCtrl.isValidBound('dailyClassCount', '-1')).toEqual(false);
+    });
+
+    it('should check if lower bound of # of days w/classes <= upper bound', function () {
+      GeneratorOptionsCtrl.userInput.minWeeklyClassCount = '';
+      GeneratorOptionsCtrl.userInput.maxWeeklyClassCount = '';
+      expect(GeneratorOptionsCtrl.isValidRange('weeklyClassCount')).toEqual(true);
+
+      GeneratorOptionsCtrl.userInput.minWeeklyClassCount = '';
+      GeneratorOptionsCtrl.userInput.maxWeeklyClassCount = '4';
+      expect(GeneratorOptionsCtrl.isValidRange('weeklyClassCount')).toEqual(true);
+
+      GeneratorOptionsCtrl.userInput.minWeeklyClassCount = '2';
+      GeneratorOptionsCtrl.userInput.maxWeeklyClassCount = '4';
+      expect(GeneratorOptionsCtrl.isValidRange('weeklyClassCount')).toEqual(true);
+
+      GeneratorOptionsCtrl.userInput.minWeeklyClassCount = '8';
+      GeneratorOptionsCtrl.userInput.maxWeeklyClassCount = '4';
+      expect(GeneratorOptionsCtrl.isValidRange('weeklyClassCount')).toEqual(false);
+
+      GeneratorOptionsCtrl.userInput.minWeeklyClassCount = '8';
+      GeneratorOptionsCtrl.userInput.maxWeeklyClassCount = '';
+      expect(GeneratorOptionsCtrl.isValidRange('weeklyClassCount')).toEqual(true);
+    });
+
+    it('should check if lower/upper bound of # of days w/classes is valid', function () {
+      expect(GeneratorOptionsCtrl.isValidBound('weeklyClassCount', '')).toEqual(true);
+      expect(GeneratorOptionsCtrl.isValidBound('weeklyClassCount', '2.5')).toEqual(false);
+      expect(GeneratorOptionsCtrl.isValidBound('weeklyClassCount', '2')).toEqual(true);
+      expect(GeneratorOptionsCtrl.isValidBound('weeklyClassCount', '8')).toEqual(false);
+      expect(GeneratorOptionsCtrl.isValidBound('weeklyClassCount', '-1')).toEqual(false);
+    });
+  });
+})();
