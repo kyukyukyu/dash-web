add86eddf0662b4744c1e8e7aff88f3dca9334b0
Sanggyu Nam <pokeplus@gmail.com>
2015-07-17 18:34:23 +0900

    structure: Conform to style Y165
    * Related to issue #14.

--- app/common/common.module.js
+++ app/common/common.module.js
@@ -2,9 +2,13 @@
 
 angular
   .module('dashApp.common', [
+    /* Angular modules */
     'ngAnimate',
     'ngCookies',
     'ngSanitize',
+    /* Cross-app modules */
+    /* 3rd-party modules */
     'restangular',
     'ui.router',
+    'ui.bootstrap',
   ]);
--- app/create/create.module.js
+++ app/create/create.module.js
@@ -10,7 +10,5 @@
  */
 angular
   .module('dashApp.create', [
-    'ngAnimate',
-    'ui.bootstrap',
     'dashApp.common'
   ]);
